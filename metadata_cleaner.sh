1d01378685f3b22c40945f7ea5bd4cf2bdc6095b
Carlos Gabriel <carlosgabriel03082005@gmail.com>
2024-08-13 08:48:28 -0300

    Support for Arch, Debian, Fedora, openSUSE systems and their respective derivatives.

--- metadata_cleaner.sh
+++ metadata_cleaner.sh
@@ -19,17 +19,45 @@ then
    exit 1
 fi
 
-#============================================================
-
 # Instalando ferramentas:
 #============================================================
 
 # Instalando exiftool:
+
+if [[ -f /etc/os-release ]]
+then
+
+. /etc/os-release
+fi
+
 if ! type exiftool > /dev/null 2>&1
 then
    echo "Instalando Exiftool, isso deve levar algum tempo..."
 
-   sudo apt-get install --reinstall libimage-exiftool-perl > /dev/null 2>&1
+   #Verificando qual o tipo do sistema
+   
+text_default="Instalando Exiftool com "
+
+   if command -v apt &> /dev/null
+   then
+      echo "$text_default apt"
+      sudo apt-get install --reinstall -y libimage-exiftool-perl > /dev/null 2>&1
+   elif command -v pacman &> /dev/null
+   then
+      echo "$text_default pacman"
+      sudo pacman -S --noconfirm perl-image-exiftool > /dev/null 2>&1
+   elif command -v dnf &> /dev/null
+   then
+      echo "$text_default dnf"
+      sudo dnf install -y perl-Image-ExifTool > /dev/null 2>&1
+   elif command -v zypper &> /dev/null
+   then
+      echo "$text_default zypper"
+      sudo zypper install -y exiftool > /dev/null 2>&1
+   else
+      echo "Erro: Nenhum gerenciador de pacotes encontrado!"
+      exit 1
+   fi
 
    echo "Prosseguindo para a proxima etapa..."
 
@@ -37,6 +65,8 @@ then
 fi
 #============================================================
 
+# limpando metadados da(s) imagem(ns)
+#============================================================
 echo -e "Limpando metadados...\n"
 
 dir="img"
@@ -49,3 +79,4 @@ for img in "$dir"/*; do
    echo -e "\nOs metadados da imagem já foram limpos!"
    fi
 done
+#============================================================
